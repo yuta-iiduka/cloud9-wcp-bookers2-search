f1d13b45d661cde0f692d0d90972d0088610b816
yuta-iiduka <ota12041204@gmail.com>
2021-02-06 06:23:59 +0000

    課題５

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,11 @@
 module SearchesHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  def current_user?(user)
+    user == current_user
+  end
+  def logged_in?
+    !current_user.nil?
+  end
 end
